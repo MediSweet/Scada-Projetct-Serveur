2f6e095b131f17f3b6b8d57eca03e0771082546b
Administrateur <w.ouldamara@gmail.com>
2025-07-01 09:07:59 +0100

    Ajouter plusieur tentative de connection au sheet et au sql

--- main.py
+++ main.py
@@ -11,23 +11,38 @@ from services.google_sheets_service.sheets_connector import get_gspread_client
 from tableToSheetMain.tableToSheetMain import table_to_sheet_main
 
 sys.path.append("C:/Users/HP/Documents/Pinku doc/Projet Python/scada/PythonProject")
- #
- # logging.basicConfig(
- #     level=logging.INFO,
- #     format="%(asctime)s - %(levelname)s - %(message)s",
- #     datefmt="%Y-%m-%d %H:%M:%S"
- # )
 
-# Crée un dossier de logs si nécessaire
+
+# Création des handlers
 log_dir = "C:\\Users\\Administrateur\\PycharmProjects\\PythonProject\\dist\\logs"
 os.makedirs(log_dir, exist_ok=True)
 
-# Configure le logger
-logging.basicConfig(
-    filename=os.path.join(log_dir, "GetDataFromScada.log"),
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Handler fichier
+file_handler = logging.FileHandler(os.path.join(log_dir, "GetDataFromScada.log"), encoding='utf-8')
+file_handler.setLevel(logging.INFO)
+
+# Handler console (stream)
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.INFO)
+
+# 💡 Forcer l'encodage UTF-8 si possible
+try:
+    sys.stdout.reconfigure(encoding='utf-8')  # Disponible à partir de Python 3.7+
+except Exception as e:
+    # Pour les versions plus anciennes, on ne fait rien ou on affiche un avertissement
+    pass
+
+# Format des logs
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
 client = get_gspread_client()
 def main():
     conn = connect_sqlserver()
